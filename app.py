97e8993ae9132ab5162d31b01718df132ac16c0e
Adamya Kumar <101924838+Adamya-Kumar@users.noreply.github.com>
2025-04-15 12:11:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+i=1
+while i <= 3:
+    commad = input(">")
+    commad=commad.lower()
+    if commad == "help":
+        print('start - to start the car')
+        print('stop - to stop the car')
+        print('quit - to exit')
+        user_commad = ''
+        flag=-1
+        while user_commad != 'quit':
+            user_commad=input(">")
+            user_commad=user_commad.lower()
+            if user_commad == 'start' :
+                if flag == 1:
+                    print("Car already started..")
+                    continue
+                print("Car started...Ready to go!")
+                flag=1
+            elif user_commad == 'stop' :
+                if flag == 0:
+                    print("Car already  stoped")
+                    continue
+
+                print("Car stopped")
+                flag=0
+            elif user_commad == 'quit':
+                flag=-1
+                commad = user_commad
+            else:
+                print("I don't understand that...")
+        break
+    else:
+        print("Try Again , type help to start ")
+    i+=1
+if i == 3:
+    print("Limit reach load the progam")
